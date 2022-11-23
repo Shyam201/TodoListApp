83dccffaff6edf72c4d0b7805e9fa76a8bbe2778
shyam sankar <shyamsankar636@gmail.com>
2022-11-23 23:01:24 +0530

    edit added

--- main.py
+++ main.py
@@ -2,16 +2,36 @@
 
 todos = []
 
-while 1 < 6 :
+while 1 < 6:
 
-    user_choice = input ("Type 'add' or 'show' : ")
-    match user_choice :
+    user_choice = input("Type 'add' or 'show' or 'exit' or 'edit' : ")
+
+    match user_choice:
         case 'add':
             todo = input("Enter a todo :").capitalize()
             todos.append(todo)
 
-        case 'show' :
-            print(todos)
+        case 'show':
+            for item in todos:
+                print(item)
+
+        case 'edit':
+            index = int(input("enter the number of the todo to edit :"))
+
+            index = index - 1
+            index_item = todos[index]
+            print(f"You are going to edit {index_item}")
+            new_todo = input("Enter new todo :")
+            todos[index] = new_todo
+
+
+        case 'exit':
+            break
+
+        case _:
+            print("Sorry you entered wrong command. Please check your input !!!!")
+
+print("Bye...")
 
 
 
