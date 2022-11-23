509f52b14c109b1097ac9ffa3a75ba6e40cc809f
shyam sankar <shyamsankar636@gmail.com>
2022-11-24 02:57:39 +0530

    error handling

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ while True:
 
 
     #add item
-    if 'add' in user_choice:
+    if user_choice.startswith("add"):
         todo = user_choice[4:]
 
         #adding  to file
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo + '\n')
 
         #adding new items to file without overriding
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
     # show the complte todo list
-    elif 'show' in user_choice:
+    elif user_choice.startswith("show"):
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
 
@@ -33,40 +33,47 @@ while True:
             print(row)
 
     #to edit the todo list
-    elif 'edit' in user_choice:
-        index = int(user_choice[5:])
-        index = index - 1
+    elif user_choice.startswith("edit"):
+        try:
+            index = int(user_choice[5:])
+            index = index - 1
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-        new_todo = input("Enter new todo :")
-        todos[index] = new_todo + '\n'
+            new_todo = input("Enter new todo :")
+            todos[index] = new_todo + '\n'
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
 
-
+        except ValueError:
+            continue
 
     #to show complete list with indexes
-    elif 'complete' in user_choice:
-        number = int(user_choice[9:])
+    elif user_choice.startswith("complete"):
+        try:
+            number = int(user_choice[9:])
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-        index = number - 1
-        todo_to_remove = todos[index].strip('\n')
-        todos.pop(index)
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
 
-        message = f"This Todo {todo_to_remove} was removed from the list"
-        print(message)
+            message = f"This Todo {todo_to_remove} was removed from the list"
+            print(message)
+
+        except IndexError:
+            print("There is no item with that number")
+            continue
 
     #to end the programme
-    elif 'exit' in user_choice:
+    elif user_choice.startswith("exit"):
         break
 
     else:
